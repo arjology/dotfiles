05266c4c88954293eeeea2add469caa52304f3ac
Arjang Talattof <talattof@gmail.com>
2019-09-04 20:03:32 +0100

    arch linux laptop branch

---
+++ emacs/.emacs.d/elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "ff7c31abfd110e769e422d5005a8a41b21153fd6")
