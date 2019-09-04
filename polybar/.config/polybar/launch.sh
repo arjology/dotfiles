51360077e9b42ac96085c12334acfca876462455
Arjang Talattof <talattof@gmail.com>
2019-09-04 19:56:13 +0100

    added confs for arch linux laptop

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,20 +7,6 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-#polybar example &
-#polybar -c ${HOME}/.config/polybar/config2 laptop &
-polybar -c ${HOME}/.config/polybar/config2 centerLeft &
-polybar -c ${HOME}/.config/polybar/config2 centerMiddle &
-polybar -c ${HOME}/.config/polybar/config2 centerRight &
-polybar -c ${HOME}/.config/polybar/config2 rightLeft &
-polybar -c ${HOME}/.config/polybar/config2 rightRight &
-#polybar -c ${HOME}/.config/polybar/config_3 title &
-#polybar -c ${HOME}/.config/polybar/config_3 cpu &
-#polybar -c ${HOME}/.config/polybar/config_3 memory & 
-#polybar -c ${HOME}/.config/polybar/config_3 temperature &
-#polybar -c ${HOME}/.config/polybar/config_3 workspace &
-#polybar -c ${HOME}/.config/polybar/config_3 volume &
-#polybar -c ${HOME}/.config/polybar/config_3 clock &
-#polybar -c ${HOME}/.config/polybar/config_3 tray &
+polybar bottom &
 
 echo "Bars launched..."
