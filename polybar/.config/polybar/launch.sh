534426a5bba9a68e56378e0cb452dc54a8c75fb2
Arjang Talattof <talattof@gmail.com>
2021-01-14 11:34:57 +0000

    saving from fedora32 box

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,29 +1,13 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-#polybar example &
-#polybar -c ${HOME}/.config/polybar/config2 laptop &
-# polybar -c ${HOME}/.config/polybar/config centerLeft &
-# polybar -c ${HOME}/.config/polybar/config centerMiddle &
-# polybar -c ${HOME}/.config/polybar/config centerRight &
-# polybar -c ${HOME}/.config/polybar/config rightLeft &
-# polybar -c ${HOME}/.config/polybar/config rightRight &
-#polybar -c ${HOME}/.config/polybar/config_3 title &
-#polybar -c ${HOME}/.config/polybar/config_3 cpu &
-#polybar -c ${HOME}/.config/polybar/config_3 memory & 
-#polybar -c ${HOME}/.config/polybar/config_3 temperature &
-#polybar -c ${HOME}/.config/polybar/config_3 workspace &
-#polybar -c ${HOME}/.config/polybar/config_3 volume &
-#polybar -c ${HOME}/.config/polybar/config_3 clock &
-#polybar -c ${HOME}/.config/polybar/config_3 tray &
-
 polybar top &
-polybar bottom &
+polybar bottom;
 
 echo "Bars launched..."
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -2,4 +2,10 @@ autoload zmv
 export CUDA_HOME=/usr/local/cuda
 export CUDA_ROOT=$CUDA_HOME/bin/
 export PATH=${CUDA_HOME}/bin:${PATH}
+export PATH="/usr/local/jvm/java/bin":$PATH
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+export TERM=xterm-24bit
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+alias fb_fix="/usr/local/bin/cc-certs && sudo soloctl -i"
